e0fece8959d33e0b06cb375d52be432cbd50aed3
Plogbilen <philip3412@gmail.com>
2016-03-23 22:13:52 +0100

    First

---
+++ FlygplatsCS2/FlygplatsCS2/Flight.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlygplatsCS2
+{
+    class Flight
+    {
+        private String _flightnumber;
+        private int _numberOfPassengers;
+        private int _fuelLevel;
+
+        public string Name
+        {
+            get { return _flightnumber; }
+            set { _flightnumber = value; }
+        }
+
+        public int numberOfPassengers
+        {
+            get { return _numberOfPassengers; }
+            set { _numberOfPassengers = value; }
+        }
+
+        public int fuelLevel
+        {
+            get { return _fuelLevel; }
+            set { _fuelLevel = value; }
+        }
+    }
+}
