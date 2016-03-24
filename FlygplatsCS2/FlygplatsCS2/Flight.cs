62e6bd6175ad1fcb206f118e2ac6b709bf815537
Plogbilen <philip3412@gmail.com>
2016-03-24 15:49:34 +0100

    More GUI stuff

--- FlygplatsCS2/FlygplatsCS2/Flight.cs
+++ FlygplatsCS2/FlygplatsCS2/Flight.cs
@@ -8,9 +8,19 @@ namespace FlygplatsCS2
 {
     class Flight
     {
+        Random rnd = new Random();
+
         private String _flightnumber;
         private int _numberOfPassengers;
         private int _fuelLevel;
+
+        public Flight()
+        {
+            _fuelLevel = rnd.Next(0, 11);
+            _flightnumber = RandomString(8);
+        }
+
+        
 
         public string Name
         {
@@ -29,5 +39,26 @@ namespace FlygplatsCS2
             get { return _fuelLevel; }
             set { _fuelLevel = value; }
         }
+
+        public bool hasCrashed()
+        {
+            if (rnd.Next(0, 241) == 0)
+                return true;
+            return false;
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        public void updateFuelLevels()
+        {
+            _fuelLevel--;
+        }
+
     }
 }
--- FlygplatsCS2/FlygplatsCS2/MainWindow.xaml.cs
+++ FlygplatsCS2/FlygplatsCS2/MainWindow.xaml.cs
@@ -20,14 +20,27 @@ namespace FlygplatsCS2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
         public MainWindow()
         {
             InitializeComponent();
+            this.Title = "Airport Simulator";
+        }
+
+        private bool plane_appears()
+        {
+            if ((rnd.Next(0, 11)) == 0)
+                return true;
+            return false;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            lapsLbl.Content = "Hello";
+            List<Flight> incomingFlights = new List<Flight>();
+            List<Flight> outgoingFlights = new List<Flight>();
+            int used = 0, unused = 0, occ = 0, laps = 0, incomingCrashedLowFuel = 0, incomingCrashedAccident = 0, outgoingCrashedAccident = 0, planesOut = 0, planesIn = 0;
+            bool occupied = false;
+            Flight f = new Flight();
         }
     }
 }
