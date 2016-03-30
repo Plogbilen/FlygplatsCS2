d0357e0c87157a1098b22cdf9f4386a961579fdb
Plogbilen <philip3412@gmail.com>
2016-03-30 09:59:32 +0200

    More logic

--- FlygplatsCS2/FlygplatsCS2/MainWindow.xaml.cs
+++ FlygplatsCS2/FlygplatsCS2/MainWindow.xaml.cs
@@ -40,6 +40,79 @@ namespace FlygplatsCS2
         int incFlights = 0, outFlights = 0, occfor = 0, laps = 0;
         bool occupied = false;
 
+        public void updateLbl1()
+        {
+            incFlightNr1.Content = incomingFlights[0].flightNumber;
+            incFlightFuelLvl1.Content = incomingFlights[0].fuelLevel;
+            incFlightPsgC1.Content = incomingFlights[0].numberOfPassengers;
+        }
+
+        public void updateLbl2()
+        {
+            updateLbl1();
+            incFlightNr2.Content = incomingFlights[1].flightNumber;
+            incFlightFuelLvl2.Content = incomingFlights[1].fuelLevel;
+            incFlightPsgC2.Content = incomingFlights[1].numberOfPassengers;
+        }
+
+        public void updateLbl3()
+        {
+            updateLbl2();
+            incFlightNr3.Content = incomingFlights[2].flightNumber;
+            incFlightFuelLvl3.Content = incomingFlights[2].fuelLevel;
+            incFlightPsgC3.Content = incomingFlights[2].numberOfPassengers;
+        }
+
+        public void updateLbl4()
+        {
+            updateLbl3();
+            incFlightNr4.Content = incomingFlights[3].flightNumber;
+            incFlightFuelLvl4.Content = incomingFlights[3].fuelLevel;
+            incFlightPsgC4.Content = incomingFlights[3].numberOfPassengers;
+        }
+
+        public void updateLbl5()
+        {
+            updateLbl4();
+            incFlightNr5.Content = incomingFlights[4].flightNumber;
+            incFlightFuelLvl5.Content = incomingFlights[4].fuelLevel;
+            incFlightPsgC5.Content = incomingFlights[4].numberOfPassengers;
+
+        }
+
+        public void updateLbl6()
+        {
+            updateLbl5();
+            incFlightNr6.Content = incomingFlights[5].flightNumber;
+            incFlightFuelLvl6.Content = incomingFlights[5].fuelLevel;
+            incFlightPsgC6.Content = incomingFlights[5].numberOfPassengers;
+
+        }
+
+        public void updateLbl7()
+        {
+            updateLbl6();
+            incFlightNr7.Content = incomingFlights[6].flightNumber;
+            incFlightFuelLvl7.Content = incomingFlights[6].fuelLevel;
+            incFlightPsgC7.Content = incomingFlights[6].numberOfPassengers;
+        }
+
+        public void updateLbl8()
+        {
+            updateLbl7();
+            incFlightNr8.Content = incomingFlights[7].flightNumber;
+            incFlightFuelLvl8.Content = incomingFlights[7].fuelLevel;
+            incFlightPsgC8.Content = incomingFlights[7].numberOfPassengers;
+        }
+
+        public void updateLbl9()
+        {
+            updateLbl8();
+            incFlightNr9.Content = incomingFlights[8].flightNumber;
+            incFlightFuelLvl9.Content = incomingFlights[8].fuelLevel;
+            incFlightPsgC9.Content = incomingFlights[8].numberOfPassengers;
+        }
+
         public void timerTick(object sender, EventArgs e)
         {
             lapsValLbl.Content = ++laps;
@@ -57,51 +130,39 @@ namespace FlygplatsCS2
             switch(incomingFlights.Count)
             {
                 case 1:
-                    incFlightNr1.Content = incomingFlights[0].flightNumber;
-                    incFlightFuelLvl1.Content = incomingFlights[0].fuelLevel;
-                    incFlightPsgC1.Content = incomingFlights[0].numberOfPassengers;
+                    updateLbl1();
                     break;
 
                 case 2:
-                    incFlightNr1.Content = incomingFlights[0].flightNumber;
-                    incFlightFuelLvl1.Content = incomingFlights[0].fuelLevel;
-                    incFlightPsgC1.Content = incomingFlights[0].numberOfPassengers;
-
-                    incFlightNr2.Content = incomingFlights[1].flightNumber;
-                    incFlightFuelLvl2.Content = incomingFlights[1].fuelLevel;
-                    incFlightPsgC2.Content = incomingFlights[1].numberOfPassengers;
+                    updateLbl2();
                     break;
 
                 case 3:
-                    incFlightNr1.Content = incomingFlights[0].flightNumber;
-                    incFlightFuelLvl1.Content = incomingFlights[0].fuelLevel;
-                    incFlightPsgC1.Content = incomingFlights[0].numberOfPassengers;
-
-                    incFlightNr2.Content = incomingFlights[1].flightNumber;
-                    incFlightFuelLvl2.Content = incomingFlights[1].fuelLevel;
-                    incFlightPsgC2.Content = incomingFlights[1].numberOfPassengers;
-
-                    incFlightNr3.Content = incomingFlights[2].flightNumber;
-                    incFlightFuelLvl3.Content = incomingFlights[2].fuelLevel;
-                    incFlightPsgC3.Content = incomingFlights[2].numberOfPassengers;
+                    updateLbl3();
                     break;
 
                 case 4:
-                    incFlightNr1.Content = incomingFlights[0].flightNumber;
-                    incFlightFuelLvl1.Content = incomingFlights[0].fuelLevel;
-                    incFlightPsgC1.Content = incomingFlights[0].numberOfPassengers;
+                    updateLbl4();
+                    break;
 
-                    incFlightNr2.Content = incomingFlights[1].flightNumber;
-                    incFlightFuelLvl2.Content = incomingFlights[1].fuelLevel;
-                    incFlightPsgC2.Content = incomingFlights[1].numberOfPassengers;
+                case 5:
+                    updateLbl5();
+                    break;
 
-                    incFlightNr3.Content = incomingFlights[2].flightNumber;
-                    incFlightFuelLvl3.Content = incomingFlights[2].fuelLevel;
-                    incFlightPsgC3.Content = incomingFlights[2].numberOfPassengers;
+                case 6:
+                    updateLbl6();
+                    break;
 
-                    incFlightNr4.Content = incomingFlights[3].flightNumber;
-                    incFlightFuelLvl4.Content = incomingFlights[3].fuelLevel;
-                    incFlightPsgC4.Content = incomingFlights[3].numberOfPassengers;
+                case 7:
+                    updateLbl7();
+                    break;
+
+                case 8:
+                    updateLbl8();
+                    break;
+
+                case 9:
+                    updateLbl9();
                     break;
 
                 default:
